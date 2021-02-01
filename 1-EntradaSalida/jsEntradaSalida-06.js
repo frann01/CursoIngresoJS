8534fbc3fd091e4fa6592af38a11ac9bbcc90c86
frann01 <franszellner@gmail.com>
2021-02-01 19:00:43 -0300

    Resuelto

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,9 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	let numero1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	let numero2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+	let resultado = numero1 + numero2;
+	alert("La suma es: " + resultado);
 }
 
