0aea2c6f567afbd7f94e6c980f5167d4ddf99291
frann01 <franszellner@gmail.com>
2021-02-07 16:13:02 -0300

    resuelto

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edadIng;
+
+	edadIng = document.getElementById('txtIdEdad').value;
+	edadIng = parseInt(edadIng);
+
+	if(edadIng == 15)
+	{
+		alert("Niña bonita");
+	}
+	else
+	{
+		alert("no");
+	}
 
 }//FIN DE LA FUNCIÓN
