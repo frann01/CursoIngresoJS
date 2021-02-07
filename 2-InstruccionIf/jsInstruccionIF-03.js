f88f0e2c04290b47ab55355d3d4a28738eda2c7d
frann01 <franszellner@gmail.com>
2021-02-07 16:16:17 -0300

    resuelto

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edadIng;
+
+	edadIng = document.getElementById('txtIdEdad').value;
+	edadIng = parseInt(edadIng);
+
+	if(edadIng < 18)
+	{
+		alert("Es menor de edad");
+	}
+	else
+	{
+		alert("Es mayor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
