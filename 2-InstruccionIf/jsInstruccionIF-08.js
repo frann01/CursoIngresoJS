2437bd2d3b74e0ad4458cef2288a606713a64962
frann01 <franszellner@gmail.com>
2021-02-08 11:50:39 -0300

    resuelto

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edadIng;
+	let estadoCiv;
+	edadIng = document.getElementById('txtIdEdad').value;
+	estadoCiv = document.getElementById('estadoCivil').value;
+	edadIng = parseInt(edadIng);
+
+	if(edadIng > 17 && estadoCiv == "Soltero")
+	{
+
+		alert("Es soltero y no es menor");
+	}
 
 
 }//FIN DE LA FUNCIÓN
