0372726f5e8c25462187d3d458225a9c6fa6f53d
frann01 <franszellner@gmail.com>
2021-02-15 18:31:22 -0300

    Resuelto2

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,24 @@
 function mostrar()
 {
 	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	
+	switch(destinoIngresado)
+	{
+		case "Bariloche":
+		alert("Se encuentra al oeste");
+		break;
+
+		case "Cataratas":
+		alert("Se encuentra al norte");
+		break;
+
+		case "Ushuaia":
+		alert("Se encuentra al sur");
+		break;
+
+		case "Mar del plata":
+		alert("Se encuentra al este");
+		break;
+	}
 
 }//FIN DE LA FUNCIÓN
