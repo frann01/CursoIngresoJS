1fcfcdf3d993f8527a1804dde851c6b9418a30f8
frann01 <franszellner@gmail.com>
2021-02-18 20:07:03 -0300

    Resuelto

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,62 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada = txtIdEstacion.value;
+	var destinoIngresado = txtIdDestino.value;
+	var mensaje;
+
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+		switch(destinoIngresado)
+		{
+			case "Bariloche":
+			mensaje = "Se viaja";
+			break;
+
+			default:
+			mensaje = "No se viaja";
+			break;
+		}
+		break;
+
+		case "Verano":
+		switch(destinoIngresado)
+		{
+			case "Mar del plata":
+			case "Cataratas":
+			mensaje = "Se viaja";
+			break;
+
+			default:
+			mensaje = "No se viaja";
+			break;
+		}
+		break;
+
+		case "Otoño":
+		switch(destinoIngresado)
+		{
+			default:
+			mensaje = "Se viaja";
+			break;
+		}
+		break;
+
+		case "Primavera":
+		switch(destinoIngresado)
+		{
+			case "Bariloche":
+			mensaje = "No se viaja";
+			break;
+			
+			default:
+			mensaje = "Se viaja";
+			break;
+		}
+		break
+		
+	}
+
+	alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
