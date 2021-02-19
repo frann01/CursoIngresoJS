996f2a6fcf5ebe29cca393993ec4c44b59ab4c2e
frann01 <franszellner@gmail.com>
2021-02-19 08:55:56 -0300

    Correccion

--- 3-InstruccionSwitch/jsInstruccionSwitch-06.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-06.js
@@ -4,39 +4,18 @@ function mostrar()
 	var horaDelDia =txtIdHora.value;
 	horaDelDia = parseInt(horaDelDia);
 	
-	switch(horaDelDia)
+	switch(true)
 	{
-		case 7:
-		case 8:
-		case 9:
-		case 10:
-		case 11:
+		case horaDelDia > 6 && horaDelDia < 12:
 		alert("Es de mañana");
 		break;
 
-		case 12:
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-		case 17:
-		case 18:
-		case 19:
+		case horaDelDia > 11 && horaDelDia < 20:
 		alert("Es de tarde");
 		break;
 
-		case 20:
-		case 21:
-		case 22:
-		case 23:
-		case 24:
-		case 0:
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-		case 5:
-		case 6:
+		case horaDelDia > 19 && horaDelDia < 25:
+		case horaDelDia >= 0 && horaDelDia < 7:
 		alert("Es de noche");
 		break;
 
