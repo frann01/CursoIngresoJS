0e4dcb7aa27cdac11aab870977eedf6e1cd92a03
frann01 <franszellner@gmail.com>
2021-02-19 11:18:16 -0300

    Resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,12 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let contador;
+	contador = 1;
+	while(contador < 11)
+	{
+		alert(contador);
+		contador++;
+	}
+
 }//FIN DE LA FUNCIÓN
