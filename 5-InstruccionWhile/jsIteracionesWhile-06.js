bf1b6bb99cb2e6f74d2531aa15059f3e67119cdd
frann01 <franszellner@gmail.com>
2021-02-19 11:52:27 -0300

    Resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -3,11 +3,24 @@ function mostrar()
 	var contador;
 	var acumulador;
 	var numeroIngresado;
+	var resultado;
 
-	contador=0;
-	acumulador=0;
+	contador = 0;
+	acumulador = 0;
+	resultado = 0;
+
+	while(contador < 5)
+    {
+    	acumulador = prompt("Ingrese un numero");
+    	acumulador = parseInt(acumulador); 
+    	resultado = resultado + acumulador;
+    	contador++;
+    }
 	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	resultado = parseInt(resultado);
+
+	txtIdSuma.value = resultado;
+	txtIdPromedio.value = resultado/5;
+
+
 }//FIN DE LA FUNCIÓN
