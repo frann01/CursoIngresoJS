59cdfd09d7199a9718bf39c6a5a667775dc8d8e8
frann01 <franszellner@gmail.com>
2021-02-19 11:27:21 -0300

    resuelto

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -3,6 +3,15 @@ al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
 	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	var clave;
+
+	clave = "utn750";
+
+	while(claveIngresada != "utn750")
+	{
+		claveIngresada = prompt("ingrese el número clave.");
+	}
+	
+	alert("Clave correcta");
 	
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -4,6 +4,15 @@ pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
 	var numeroIngresado;
+
 	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+
+	while(numeroIngresado > 9 || numeroIngresado < 0)
+	{
+		numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	}
+
+	txtIdNumero.value = numeroIngresado;
+	
 	
 }//FIN DE LA FUNCIÓN
