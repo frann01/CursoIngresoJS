7097b3580297b103ebdcf037b7595a1de564245b
frann01 <franszellner@gmail.com>
2021-02-23 12:34:13 -0300

    Cambios

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,32 +1,61 @@
-/*
-Al presionar el botón pedir  números  hasta que el usuario quiera,
-mostrar el número máximo y el número mínimo ingresado.*/
+
 function mostrar()
 {	
 	var banderaDelPrimero;
 	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	banderaDelPrimero=true;
-	respuesta=true;
-
-	while(respuesta == true)
+	var negativoMaximo;
+	var parMinimo;
+	var contador;
+	var banderaNegativos=true;
+	var banderaPares=true;
+	contador=0;
+	
+	while(contador < 10)
 	{
 		numeroIngresado = prompt("Ingrese un numero");
 		numeroIngresado = parseInt(numeroIngresado);
 
-		if (banderaDelPrimero == true || numeroIngresado < numeroMinimo) 
+		while(numeroIngresado == 0)
 		{
-			numeroMinimo = numeroIngresado;		
+			numeroIngresado = prompt("Error: Ingrese un numero distinto de cero");
+			numeroIngresado = parseInt(numeroIngresado);
 		}
-		if(banderaDelPrimero == true || numeroIngresado > numeroMaximo)
+		
+		if(numeroIngresado % 2 == 0)
 		{
-			numeroMaximo = numeroIngresado;
-			banderaDelPrimero = false;
-		}
-		respuesta = confirm("desea continuar?");
+			if (numeroIngresado < parMinimo || banderaPares==true) 
+			{
+				parMinimo = numeroIngresado;
+				banderaPares=false;
+			}
+		}	
+		
+		if(numeroIngresado < 0)
+		{
+			if(banderaNegativos==true || numeroIngresado>negativoMaximo)
+			{
+				negativoMaximo=numeroIngresado;
+				banderaNegativos=false;
+			}
+		}			
+		contador++;			
 	}
-	
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimo.value=numeroMinimo;
+	if(banderaNegativos == true)
+	{
+		document.write("No hay negativos"+"<br/>");
+	}
+	else
+	{
+		document.write("Negativo maximo: "+negativoMaximo+"<br/>");
+	}
+
+	if(banderaPares == true)
+	{
+		document.write("No hay pares");
+	}
+	else
+	{
+		document.write("Par minimo: "+parMinimo+"<br/>");
+	}	
+		
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -1,15 +1,4 @@
-/*
-Al presionar el botón pedir  números  
-hasta que el usuario quiera, mostrar:
-1-Suma de los negativos.
-2-Suma de los positivos.
-3-Cantidad de positivos.
-4-Cantidad de negativos.
-5-Cantidad de ceros.
-6-Cantidad de números pares.
-7-Promedio de positivos.
-8-Promedios de negativos.
-9-Diferencia entre positivos y negativos, (positvos-negativos). */
+
 function mostrar()
 {
 	//declarar contadores y variables 
@@ -72,4 +61,4 @@ function mostrar()
 		"Promedio negativos: "+promedioNegativos+"<br/>"+
 		"Diferencia entre positivos y negativos: "+diferenciaPP+"<br/>");
 	
-}//FIN DE LA FUNCIÓN
+}
