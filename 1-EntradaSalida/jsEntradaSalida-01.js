499873b52ab757a5777d0f06235c0d9e7cd2784a
frann01 <franszellner@gmail.com>
2021-02-28 20:19:03 -0300

    resuelto2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,150 @@
 /*
-Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+2- "super chino" Se pide el ingreso de mercadería de un supermercado , hasta que el usuario quiera,se pide :
+tipo(limpieza , comestible , otros)solo estos tres tipos
+nombre del producto
+importe del producto (no mayor a 1000 pesos)
+procedencia(importado, nacional, elaborado)
+Peso (no mayor a 30 kilos)
+
+informar SOLO SI HAY 
+a) el NOMBRE del mas pesado de los comestibles
+b) el NOMBRE del  mas caro de todos los productos
+c) el NOMBRE del  mas barato de los elaborados
+d)el tipo de mercadería  que mas aparece
+e)el porcentaje de productos elaborados por sobre el total
+f) el promedio de pesos por cada tipo ingresado*/
 function mostrar()
 {
-	alert("jonsa<lmtrdhmñgfrn");
+	var tipoProducto;
+	var nombreProducto;
+	var importeProducto;
+	var procedenciaProducto;
+	var pesoProducto;
+	var respuesta=true;
+	var contadorLimpieza=0;
+	var contadorComestible=0;
+	var contadorOtros= 0;
+	var masPesadoComes;
+	var nombrePesadoComes;
+	var contadorProductos=0;
+	var mayorImporte;
+	var nombreMasCaro;
+	var nombreBaratoElaborados;
+	var precioBaratoELaborados;
+	var contadorElaborados=0;
+	var acumuladorPesoLimpieza=0;
+	var acumuladorPesoOtros=0;
+	var acumuladorPesoComestibles=0;
+	var promedioPesoLimpieza;
+	var promedioPesoOtros;
+	var promedioPesoComestibles;
+	var porcentajeELaborados;
+
+	while(respuesta == true)
+	{
+		//Ingreso de datos
+		tipoProducto = prompt("Ingrese el tipo de producto");
+		while(tipoProducto != "limpieza" && tipoProducto != "comestible" && tipoProducto != "otros")
+		{
+			tipoProducto = prompt("Ingrese el tipo de producto");
+		}
+
+		nombreProducto = prompt("Ingrese el nombre del producto");
+		
+		importeProducto = prompt("Ingrese el importe del producto");
+		importeProducto=parseInt(importeProducto);
+		while(importeProducto < 0)
+		{
+			importeProducto = prompt("Ingrese el importe del producto");
+			importeProducto=parseInt(importeProducto);
+		}
+
+		procedenciaProducto = prompt("Ingrese la procedencia de producto");
+		while(procedenciaProducto != "importado" && procedenciaProducto!= "elaborado" && procedenciaProducto != "nacional")
+		{
+			procedenciaProducto = prompt("Ingrese la procedencia de producto");
+		}
+
+		pesoProducto = prompt("Ingrese el peso del producto");
+		pesoProducto=parseInt(pesoProducto);
+		while(pesoProducto>30)
+		{
+			pesoProducto = prompt("Ingrese el peso del producto");
+			pesoProducto=parseInt(pesoProducto);
+		}
+
+		switch(tipoProducto)
+		{
+			case "limpieza":
+			contadorLimpieza++;
+			acumuladorPesoLimpieza=acumuladorPesoLimpieza+pesoProducto;
+			break;
+
+			case "comestible":
+			if(contadorComestible>0 || pesoProducto > masPesadoComes)
+			{
+				masPesadoComes=pesoProducto;
+				nombrePesadoComes=nombreProducto;
+			}
+			acumuladorPesoComestibles=acumuladorPesoComestibles+pesoProducto;
+			contadorComestible++;
+
+			break;
+
+			case "otros":
+			acumuladorPesoOtros=acumuladorPesoOtros+pesoProducto;
+			contadorOtros++;
+			break;
+		}
+
+
+		if(procedenciaProducto="elaborado")
+		{
+			if(contadorElaborados>0 || importeProducto < precioBaratoELaborados)
+			{
+				precioBaratoELaborados=importeProducto;
+				nombreBaratoElaborados=nombreProducto;
+
+			}
+			contadorElaborados++;
+		}
+
+		if(contadorProductos>0 || importeProducto > mayorImporte)
+			{
+				mayorImporte= importeProducto;
+				nombreMasCaro=nombreProducto;
+			}
+
+		contadorProductos++;
+		respuesta= confirm("Desea continuar?");
+		
+	}
+
+	porcentajeELaborados= (contadorElaborados*100)/ contadorProductos;
+	promedioPesoComestibles=acumuladorPesoComestibles/contadorComestible;
+	promedioPesoOtros=acumuladorPesoOtros/contadorOtros;
+	promedioPesoLimpieza=acumuladorPesoLimpieza/contadorLimpieza;
+
+	if(contadorComestible >  contadorLimpieza && contadorComestible > contadorOtros)
+	{
+		
+		document.write("EL tipo con mas cantidad es Comestibles"+"<br/>");
+	}
+	else
+	{
+		if(contadorOtros > contadorLimpieza && contadorOtros > contadorComestible)
+		{
+			document.write("EL tipo con mas cantidad es Otros"+"<br/>");
+		}
+		else
+		{
+			document.write("EL tipo con mas cantidad es Limpieza"+"<br/>");
+		}
+	}
+	document.write("El mas pesado de los comestibles es " + nombrePesadoComes+"<br/>");
+	document.write("El mas caro de los productos es " + nombreMasCaro+"<br/>");
+	document.write("El mas barato de los elaborados es " + nombreBaratoElaborados+"<br/>");
+	document.write("El promedio de peso de limpieza, comestibles y otros es "+promedioPesoLimpieza+", "+promedioPesoComestibles+"; "+promedioPesoOtros+"<br/>");
+	document.write("El porcentaje de elaborados es " + porcentajeELaborados+"<br/>");
 }
 
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,19 +1,217 @@
-/*
-Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+/*pedir el ingreso de 10 mascotas 
+ validar 
+tipo ( gato , perro , pájaro y otros)solo esos cuatro tipo
+raza , si es perro (pastor, toy, callejero) y si gato (siamés, turco, Peterbald , generico) y  si es de tipo "otros" o pájaro , pedir solo un texto
+edad entre 1 y 20 para gatos y perros ,  para "otros" entre 1 y 100 , para pájaro 1 de 50
+nombre ( no se permite solo numero)
+mostrar por cada tipo el nombre del  mas viejo , solo si hay de ese tipo
+mostrar que raza  de gatos tiene mas animales y el promedio de edad de esta raza
+*/
 function mostrar()
 {
-	
-	let nombreAlumno = prompt("Introdusca su nombre");
+	var cantidadMascotas=0;
+	var tipoMacota;
+	var raza;
+	var edadIngresada;
+	var nombreIngresado;
+	var nombreViejoPerro;
+	var nombreViejogatos;
+	var nombreViejoPajaros;
+	var nombreViejoOtros;
+	var contadorPerros=0;
+	var contadorGatos=0;
+	var contadorPajaros=0;
+	var contadorOtros=0;
+	var EdadPerro;
+	var EdadGatos;
+	var EdadPajaros;
+	var EdadOtros;
+	var contadorsiamés=0;
+	var contadorturco=0;
+	var contadorPeterbald=0;
+	var contadorgenerico=0;
+	var acumuladorsiamés=0;
+	var acumuladorturco=0;
+	var acumuladorPeterbald=0;
+	var acumuladorgenerico=0;
+	var promediosiamés;
+	var promedioturco;
+	var promedioPeterbald;
+	var promediogenerico;
 
-	if(nombreAlumno == "")
+
+	while(cantidadMascotas<10)
 	{
-		alert("No ingreso su nombre");
+		tipoMacota = prompt("Ingrese el tipo de mascota");
+		while(tipoMacota != "gato" && tipoMacota != "perro" && tipoMacota != "pajaro" && tipoMacota != "otros")
+		{
+			tipoMacota = prompt("Ingrese el tipo de mascota");
+		}
+		nombreIngresado = prompt("Ingrese el nombre");
+		
+		switch(tipoMacota)
+		{
+
+			case "perro":
+				raza = prompt("Ingrese la raza");
+				while(raza != "pastor" && raza != "toy" && raza != "callejero")
+				{	
+					raza = prompt("Ingrese la raza");
+				}
+
+				edadIngresada = prompt("Ingrese la edad");
+				edadIngresada=parseInt(edadIngresada);
+				while(edadIngresada > 20 || edadIngresada < 1)
+				{
+					edadIngresada = prompt("Ingrese la edad");
+					edadIngresada=parseInt(edadIngresada);
+				}
+				if(contadorPerros == 0 || edadIngresada > EdadPerro)
+				{
+					EdadPerro=edadIngresada;
+					nombreViejoPerro=nombreIngresado;
+				}
+				contadorPerros++;
+				break;
+
+			case "gato":
+				raza = prompt("Ingrese la raza");
+				while(raza != "siamés" && raza != "turco" && raza != "Peterbald" && raza != "generico")
+				{	
+					raza = prompt("Ingrese la raza");
+				}
+
+				edadIngresada = prompt("Ingrese la edad");
+				edadIngresada=parseInt(edadIngresada);
+				while(edadIngresada > 20 || edadIngresada < 1)
+				{
+					edadIngresada = prompt("Ingrese la edad");
+					edadIngresada=parseInt(edadIngresada);
+				}
+
+				if(contadorGatos == 0 || edadIngresada > EdadGatos)
+				{
+					EdadGatos=edadIngresada;
+					nombreViejogatos=nombreIngresado;
+				}
+
+				switch(raza)
+				{
+
+					case "siamés":
+					acumuladorsiamés=acumuladorsiamés+edadIngresada;
+					contadorsiamés++;
+					break;
+
+					case "turco":
+					acumuladorturco=acumuladorturco+edadIngresada;
+					contadorturco++;
+					break;
+
+					case "Peterbald":
+					acumuladorPeterbald=acumuladorPeterbald+edadIngresada;
+					contadorPerros++;
+					break;
+
+					case "generico":
+					acumuladorgenerico=acumuladorgenerico+edadIngresada;
+					contadorgenerico++;
+					break;		
+				} 
+
+				contadorGatos++;
+				break;
+
+
+			case "pajaro":
+				raza = prompt("Ingrese la raza");
+				edadIngresada = prompt("Ingrese la edad");
+				edadIngresada=parseInt(edadIngresada);
+				while(edadIngresada > 50 || edadIngresada < 1)
+				{
+					edadIngresada = prompt("Ingrese la edad");
+					edadIngresada=parseInt(edadIngresada);
+				}
+
+				if(contadorPajaros == 0 || edadIngresada > EdadPajaros)
+				{
+					EdadPajaros=edadIngresada;
+					nombreViejoPajaros=nombreIngresado;
+				}
+				contadorPajaros++;
+				break;	
+
+
+			case "otros":
+				raza = prompt("Ingrese la raza");
+				edadIngresada = prompt("Ingrese la edad");
+				edadIngresada=parseInt(edadIngresada);
+				while(edadIngresada > 100 || edadIngresada < 1)
+				{
+					edadIngresada = prompt("Ingrese la edad");
+					edadIngresada=parseInt(edadIngresada);
+				}
+
+				if(contadorOtros == 0 || edadIngresada > EdadOtros)
+				{
+					EdadOtros=edadIngresada;
+					nombreViejoOtros=nombreIngresado;
+				}
+				contadorOtros++;
+				break;		
+		}
+		
+		
+		cantidadMascotas++;
+	}
+
+
+	promediosiamés=acumuladorgenerico/contadorsiamés;
+	promedioturco=acumuladorturco/contadorturco;	
+	promedioPeterbald=acumuladorPeterbald/contadorPeterbald;	
+	promediogenerico=acumuladorgenerico/contadorgenerico;
+
+
+	if(contadorPerros>0)
+	{
+		document.write("La edad del perro mas viejo es "+EdadPerro+" y su nombre es "+nombreViejoPerro+"<br/>");
+	}
+	if(contadorGatos>0)
+	{
+		document.write("La edad del gato mas viejo es "+EdadGatos+" y su nombre es "+nombreViejogatos+"<br/>");
+	}
+	if(contadorPajaros>0)
+	{
+		document.write("La edad del pajaro mas viejo es "+EdadPajaros+" y su nombre es "+nombreViejoPajaros+"<br/>");
+	}
+	if(contadorOtros>0)
+	{
+		document.write("La edad del otro mas viejo es "+EdadOtros+" y su nombre es "+nombreViejoPajaros+"<br/>");
+	}
+
+
+
+	if(contadorsiamés >  contadorturco && contadorsiamés > contadorPeterbald && contadorsiamés > contadorgenerico)
+	{
+		document.write("La raza con mas gatos es la siames y su promedio de edad es "+ promediosiamés+"<br/>");
 	}
 	else
 	{
-		alert("Su nombre es " + nombreAlumno);
-	}
-	
+		if(contadorPeterbald > contadorturco && contadorPeterbald > contadorsiamés && contadorPeterbald > contadorgenerico)
+		{
+			document.write("La raza con mas gatos es la peterbald y su promedio de edad es "+promedioPeterbald+"<br/>");
+		}
+		else
+		{
+			if(contadorturco > contadorPeterbald && contadorturco > contadorsiamés && contadorturco > contadorgenerico)
+			{
+				document.write("La raza con mas gatos es la turca y su promedio de edads ed "+promedioturco+ "<br/>");
+			}
+			else
+			{
+				document.write("La raza con mas gatos es la generica "+promediogenerico+"<br/>");
+			}
+		}
+	}		
 }
 
