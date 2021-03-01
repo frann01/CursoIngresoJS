34a90d2605d2376423baa68dcd2ae7baa995b43d
frann01 <franszellner@gmail.com>
2021-03-01 15:39:01 -0300

    Ej Parcial

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,12 +1,129 @@
-/*
-Debemos lograr tomar un dato por 'ID'
-y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+/*Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, 
+hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.11
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.11
+a) El importe total a pagar , bruto sin descuento y...11
+b) el importe total a pagar con descuento(solo si corresponde)11
+d) Informar el tipo con mas cantidad de bolsas.11
+f) El tipo mas caro11
+*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado = document.getElementById('txtIdNombre').value;
-	alert(nombreIngresado);
+	var respuesta=true;
+	var tipoProducto;
+	var cantidadIngresada;
+	var importeProducto;
+	var acumuladorBolsas=0;
+	var totalSinDescuento=0;
+	var contadorCal=0;
+	var contadorArena=0;
+	var contadorCemento=0;
+	var acumuladorCal=0;
+	var acumuladorArena=0;
+	var acumuladorCemento=0;
+	var banderaProductos=true;
+	var mayorPrecio;
+	var tipoMayorPrecio;
+	var descuento=0;
+	var precioDescuento;
 
+	while(respuesta == true)
+	{
+		//Ingreso de datos
+		tipoProducto = prompt("Ingrese el tipo de producto");
+		tipoProducto=tipoProducto.toLowerCase();
+		while(tipoProducto != "arena" && tipoProducto != "cal" && tipoProducto != "cemento")
+		{
+			tipoProducto = prompt("Ingrese el tipo de producto");
+			tipoProducto=tipoProducto.toLowerCase();
+		}
+		
+		importeProducto = prompt("Ingrese el importe del producto");
+		importeProducto=parseInt(importeProducto);
+		while(importeProducto < 0 || isNaN(importeProducto))
+		{
+			importeProducto = prompt("Ingrese el importe del producto");
+			importeProducto=parseInt(importeProducto);
+		}
+
+		cantidadIngresada = prompt("Ingrese la cantidad de bolsas");
+		cantidadIngresada=parseInt(cantidadIngresada);
+		while(isNaN(cantidadIngresada))
+		{
+			cantidadIngresada = prompt("Ingrese la cantidad de bolsas");
+			cantidadIngresada=parseInt(cantidadIngresada);
+		}
+		acumuladorBolsas= acumuladorBolsas+cantidadIngresada;
+		totalSinDescuento= totalSinDescuento+ importeProducto;
+
+		if(banderaProductos== true || importeProducto > mayorPrecio)
+			{
+				mayorPrecio=importeProducto;
+				tipoMayorPrecio=tipoProducto;
+			}
+
+		switch(tipoProducto)
+		{
+			case "cal":
+			acumuladorCal= acumuladorCal+cantidadIngresada;
+		
+			break;
+
+			case "arena":
+			acumuladorArena=acumuladorArena+cantidadIngresada;
+
+			break;
+
+			case "cemento":
+			acumuladorCemento=acumuladorCemento+ cantidadIngresada;
+			break;
+		}
+
+		respuesta=confirm("Desea continuar?");
+	}
+
+	if(acumuladorBolsas>10 &&acumuladorBolsas<30)
+	{
+		descuento=0.15;
+	}
+	else
+	{
+		if(acumuladorBolsas>30)
+		{
+			descuento=0.25;
+		}
+	}
+
+	document.write("El total sin descuento es " + totalSinDescuento+"<br/>");
+
+	if(descuento>0)
+	{
+		precioDescuento= totalSinDescuento-(totalSinDescuento*descuento);
+		document.write("El total con descuento es " + precioDescuento+"<br/>");
+	}
+
+	if(acumuladorCal >  acumuladorCemento && acumuladorCal > acumuladorArena)
+	{
+		
+		document.write("EL tipo con mas cantidad es Cal"+"<br/>");
+	}
+	else
+	{
+		if(acumuladorArena > acumuladorArena && contadorOtros > acumuladorCal)
+		{
+			document.write("EL tipo con mas cantidad es Arena"+"<br/>");
+		}
+		else
+		{
+			document.write("EL tipo con mas cantidad es Cemento"+"<br/>");
+		}
+	}
+
+	document.write("EL tipo mas caro es "+tipoMayorPrecio+"<br/>");	
 }
 
 
